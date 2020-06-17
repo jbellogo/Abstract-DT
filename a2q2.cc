7abb9b84157d1027724f39cd95a2cbb9788d4502
jbellogo <jbnermal3@gmail.com>
2020-06-17 02:13:01 -0400

    backup semifinal

--- a2q2.cc
+++ a2q2.cc
@@ -12,14 +12,42 @@ using std::stringstream;
 using std::cin;
 
 // argv[-bt, file1,file2 ...]
-// need to include all arguments here now
-void format_infile(std::istream * inf, int & count){
-  std::string s;
-  while (getline(*inf, s)) {
-    std::cout << setw(3) << count << ": \'" << s << "\'" << std::endl;
-    count++;
+bool ba = false;
+bool bt = false;
+bool nrn = false;
+bool nrz = false;
+// better to be ints
+int w = 6;
+string s = "\t";
+
+void format_infile(std::istream * inf, int & count, bool ba, bool bt,
+  bool nrn, bool nrz, int w, string s) {
+    std::string str;
+    // ba, bt, s, w, nrn
+    std::cout << ba << bt << nrn << nrz << w << s << '\n';
+    while (getline(*inf, str)) {
+      if (ba) {
+        if (nrn) {
+          std::cout << setw(w) << count << s << str << std::endl;
+        } else if (nrz) {
+          std::cout << std::setfill ('0') << setw(w) << count;
+          std::cout << s << str << std::endl;
+        }
+      } else {
+        if (str != "") {
+          if (nrn) {
+            std::cout << setw(w) << count << s << str << std::endl;
+          } else if (nrz) {
+            std::cout << std::setfill ('0') << setw(w) << count;
+            std::cout << s << str << std::endl;
+          }
+        }
+      }
+      count++;
+    }
   }
-}
+
+
 
 bool member(string item, int length, string arr[]){
   for (int i=0; i<length; i++) {
@@ -29,15 +57,9 @@ bool member(string item, int length, string arr[]){
 }
 
 bool is_digit_greater_than_1(char a) {
-  string str{1, a};
-  for (int i = 2; i < 10; i++) {
-    stringstream ss;
-    ss << i;
-    string s;
-    ss >> s;
-    if (s == str) {
-      return true;
-    }
+  if (a == '2' || a == '3' || a == '4' || a == '5' || a == '6' ||
+  a == '7' || a == '8' || a == '9') {
+    return true;
   }
   return false;
 }
@@ -72,32 +94,33 @@ int main(int argc, char const *argv[]) {
         // deal with -args
         //////////////////
         for (; (i < argc) && (argv[i][0] == '-') ; i++) {
+          string str {argv[i]};
 
-          if (argv[i][1] == 'w') {
-            if (is_digit_greater_than_1(argv[i][2])) {
-              // will be updated to latest! i think
-              w = char_to_int(argv[i][2]);
+          if (str.substr(0, 2) == "-w") {
+            if (is_digit_greater_than_1(str[2])) {
+              // will be updated to latest!
+              w = char_to_int(str[2]);
             } else {
               std::cout << "ERROR: "<< argv[i] <<" is not a valid formatting argument" << endl;
               return 1; // END
             }
-          } else if (argv[i][1] == 's') {
-            s = argv[i];
-            s = s.substr(2);             // will be updated to latest!
+          } else if (str.substr(0,2) == "-s") {
+            s = str.substr(2);             // will be updated to latest!
             if (s == "TAB") { s = "\t"; } // hm yup
-          } else if (argv[i] == "-ba") {
+          } else if (str == "-ba") { // without the string you are comparing the addresses
             ba = true;
-          } else if (argv[i] == "-bt") {
+          } else if (str == "-bt") {
             bt = true;
-          } else if (argv[i] == "nrn") {
+          } else if (str == "-nrn") {
             nrn = true;
-          } else if (argv[i] == "nrz") {
+          } else if (str == "-nrz") {
             nrz = true;
           } else {
             std::cout << "ERROR: "<< argv[i] <<" is not a valid formatting argument" << endl;
             return 1; // END
           }
         }
+
         // once all analized for validty check for contradictions
         if (ba && bt && nrn && nrz) {
           std::cout << bb_message << endl;
@@ -110,12 +133,20 @@ int main(int argc, char const *argv[]) {
           std::cout << nn_message << endl;
           return 1; // END
         }
+
     } else {
+      // default set up
+      if (! (ba || bt)) {
+        bt = true;
+      }
+      if (! (nrn || nrz)) {
+        nrn = true;
+      }
       //////////////////
       // deal with files
       /////////////////
       std::istream *infile = &std::cin; // take from input by default
-      int count = 0;
+      int count = 1;
       for (; i < argc; i++) {
         infile = new std::ifstream{ argv[i] };   // FREE at some point
         if ( infile == nullptr || infile->fail() ) {
@@ -123,13 +154,20 @@ int main(int argc, char const *argv[]) {
           std::cout << "ERROR: " << argv[i] << " cannot be opened for input"<< endl;
           return 1;
         } else {
-          format_infile(infile, count); // outputs to stdout
+          format_infile(infile, count, ba, bt, nrn, nrz, w, s); // outputs to stdout
           delete infile;
         }
       }
       return 0;
     }
   } // main for loop
-  int count = 0;
-  format_infile(&cin, count);
+  // default set up
+  if (! (ba || bt)) {
+    bt = true;
+  }
+  if (! (nrn || nrz)) {
+    nrn = true;
+  }
+  int count = 1;
+  format_infile(&cin, count, ba, bt, nrn, nrz, w, s);
 }
